323ad585670f5fabc55279662eb761fca6d3edb4
mwong678 <matthew.wong@wisc.edu>
2019-12-09 22:39:41 -0800

    spotify

--- src/index.js
+++ src/index.js
@@ -1,16 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { Redirect } from 'react-router-dom';
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+const request = require('request'),
+      requestPromise = require('request-promise'),
+      querystring = require('querystring'),
+      properties = require('./properties.json');
+
+const clientID = properties.clientID,
+      clientSecret = properties.clientSecret,
+      redirectURI = properties.redirectURILocal,
+      scope = "user-top-read";
+
+function Welcome() {
+  let auth = false;
+  console.log(window.location.href);
+  if (!auth){
+    return (
+      <div className="Welcome">
+        <h1>Welcome to SStats</h1>
+        <a href={getAuthURL()}>Get Spotify Stats</a>
+      </div>
+    );
+  }else{
+    return (
+      <div className="Welcome">
+        <h1>Spotify Statistics</h1>
+      </div>
+    );
+  }
 }
 
+function getAuthURL(){
+  return 'https://accounts.spotify.com/authorize?' +
+  querystring.stringify({
+   response_type: 'token',
+   client_id: clientID,
+   scope: scope,
+   redirect_uri: redirectURI,
+   state: generateRandomString(16),
+   show_dialog: true
+  });
+}
+
+function generateRandomString(length){
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (let i = 0; i < length; i++) {
+   text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Welcome />, rootElement);
